29d0734f7cafa893cbd14d3868046cb876e3537e
SabinaMB <sabina.marian.blaariu@gmail.com>
2025-01-26 23:27:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-heading text-4xl pb-20"> Heading</h1>
+      <p className="text-lg">paragraf</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        heading: ["Playfair Display", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
