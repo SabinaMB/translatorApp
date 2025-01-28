a7caee75b4647df91840c46ad9160c4270a976da
SabinaMB <sabina.marian.blaariu@gmail.com>
2025-01-28 15:33:54 +0100

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import TranslatorApp from "./TranslatorApp";
 function App() {
   return (
-    <div className="w-full h-screen  bg-gray-400 flex justify-center items-center">
+    <div className="w-full min-h-screen bg-gray-400 flex justify-center items-center">
       <TranslatorApp />
     </div>
   );
